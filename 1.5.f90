b0121dfb9a7969fc4f0327b79cb8b5b383f8c4b8
p-dr <ilidio@yandex.com>
2017-03-24 07:22:57 -0300

    6

--- 1.5.f90
+++ 1.5.f90
@@ -5,38 +5,34 @@ SUBROUTINE SHIFT(RES, POS)
   DO I=M, POS+1, -1
      RES(I) = RES(I-1)
   END DO
-  PRINT*, RES
+  PRINT*, "SHIFTED"
 END SUBROUTINE SHIFT
 
 
 SUBROUTINE MENORES(N, M)
-  INTEGER N, M
   INTEGER RES(M)
   INTEGER IPT
-  RES = (/ 0,0,0/)
+  RES = (/ 99,98,97,96,95/)
   DO I=1, N
      READ*, IPT
 
      DO J=M, 1, -1
-        IF ((IPT < RES(J)) .AND. (RES(J) .NE. 0.0)) THEN
-        
-           IF (J .EQ. 1) THEN
-              PRINT*, 'IF2 CALLED'
-              CALL SHIFT(RES, 1)
-              RES(1) = IPT
-              EXIT
-           END IF
-           
-           CALL SHIFT(RES, J+1)
+        IF (RES(J) .LE. IPT) THEN
+           CALL SHIFT(RES, J)
            RES(J+1) = IPT
            PRINT*, 'IF1 CALLED'
            EXIT
-     END IF
-     
+
+        ELSE IF ((J .EQ. 1) .AND. (RES(J) > IPT)) THEN
+           PRINT*, 'IF2 CALLED'
+           CALL SHIFT(RES, 1)
+           RES(1) = IPT
+           EXIT
+        END IF
 
      END DO
      
-  PRINT*, 'RES FINAL', RES        
+  PRINT*, 'RES ', RES        
   END DO
 
 
@@ -45,7 +41,12 @@ END SUBROUTINE MENORES
 
 
 PROGRAM P
-  INTEGER RES(3)
+  INTEGER RES(5)
+  INTEGER, PARAMETER :: N=10, M = 5
   INTEGER POS
-  CALL MENORES(5,3)
+  POS = 3
+  res = (/1,2,3,4,5/)
+  CALL SHIFT(RES, 3)
+  PRINT*, RES
+  !  CALL MENORES(N,M)
 END PROGRAM P
