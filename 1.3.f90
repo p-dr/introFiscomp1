48d8c5f199edc87e390abcf2450262777e2ed2c8
p-dr <ilidio@yandex.com>
2017-03-24 14:45:19 -0300

    5

--- 1.3.f90
+++ 1.3.f90
@@ -1,48 +1,31 @@
 PROGRAM P
   REAL*16 SEN
-  F = SEN(3, 4)
-!  G = SEN(5.0, 3)
-!  H = SEN(4.0, 3)
+  F = SEN(3.0, 15)
   
 END PROGRAM P
-
-RECURSIVE FUNCTION fact(n) RESULT(res)
-
-  IMPLICIT NONE
-  INTEGER, INTENT(IN) :: n
-  INTEGER*16 res
-
-  IF (n == 0) THEN
-     res = 1
-       
-  ELSE
-     res = n * fact(n-1)
-     
-  ENDIF
-
-END FUNCTION fact
-
 
 FUNCTION sen(n, O) RESULT(res)
   REAL, INTENT(IN) :: n
   INTEGER, INTENT(IN) :: O
   REAL*16 :: res, AN, TGT, DIFF
-  INTEGER*16 J, I
+  INTEGER*16 :: J, I
 
   I=0
   TGT = SIN(N)
   RES = 0.0
-  DIFF = (ABS(RES - TGT))
+  DIF = 999999.9
   
-  DO WHILE (DIFF > 10.0**(-O))
-     DO J = I*2+1, 1, -1
-        AN = (-1.0)**(I) * N**(I*2+1)/J
+  DO WHILE (ABS(ABS(RES) - ABS(SIN(N))) > 10.0**(-O))
+!DO K=1,10
+     AN = (-1.0)**(I) * N**(I*2+1)
+     DO J = I*2+1, 2, -1
+        AN = AN/J
      END DO
      
      RES = RES + AN
-     PRINT*,'AN', AN,'RES', RES
+!     PRINT*,'AN', AN,'RES', RES, 'DIFF', DIFF
      I=I+1
-     DIFF = (ABS(RES - TGT))
+     DIFF = ABS(RES - SIN(N))
   END DO
   PRINT*, '========================================================'
   PRINT*, "RESULTADOS DA APROXIMAÇÃO PARA SIN(", N,"), COM PRECISÃO", 10.0**(-O)
--- 1.5.f90
+++ 1.5.f90
@@ -1,51 +1,54 @@
-SUBROUTINE SHIFT(RES, POS)
-  INTEGER RES(M)
-  INTEGER POS
-
+SUBROUTINE SHIFT(RES, M, POS)
+  IMPLICIT NONE
+  INTEGER M, POS, I
+  REAL RES(M)
+  
   DO I=SIZE(RES), POS+1, -1
      RES(I) = RES(I-1)
   END DO
-  PRINT*, "SHIFTED", RES
+
 END SUBROUTINE SHIFT
 
-
 SUBROUTINE MENORES(N, M)
-  INTEGER RES(M)
-  INTEGER IPT
-  RES = (/ 99,98,97,96,95/)
+  REAL RES(M), IPT
+  INTEGER M, N
+  RES = (/(K, K=999999999,999999999-N,-1)/)
   DO I=1, N
      READ*, IPT
 
      DO J=M, 1, -1
         IF (RES(J) .LE. IPT) THEN
-           CALL SHIFT(RES, J)
+           CALL SHIFT(RES, N, J)
            RES(J+1) = IPT
-           PRINT*, 'IF1 CALLED'
+           !PRINT*, 'IF1 CALLED'
            EXIT
 
         ELSE IF ((J .EQ. 1) .AND. (RES(J) > IPT)) THEN
-           PRINT*, 'IF2 CALLED'
-           CALL SHIFT(RES, 1)
+           !PRINT*, 'IF2 CALLED'
+           !CALL SHIFT(RES, N, 1)
            RES(1) = IPT
            EXIT
         END IF
 
      END DO
-     
-  PRINT*, 'RES ', RES        
   END DO
+  PRINT*, M, 'MENORES NUMEROS INSERIDOS:', RES
 
-
-  
 END SUBROUTINE MENORES
+  
 
 
 PROGRAM P
-  INTEGER RES(5)
- ! INTEGER, PARAMETER :: N=10, M = 5
-  !INTEGER POS
+  IMPLICIT NONE
+  INTEGER, PARAMETER :: N=8, M = 7
+  REAL RES(M)
+  INTEGER POS
+  !  CALL SHIFT(RES, 5, 2)
 
-  res = (/1,2,3,4,5/)
-  CALL SHIFT(RES, 3)
-  !  CALL MENORES(N,M)
+do while (.true.)
+  PRINT*, 'INPUTS:'
+  CALL MENORES(N,M)
+  print*, '============================================'
+end do
+
 END PROGRAM P
