45eb259260f302da2a6c96080ad87169d4d9d4f2
p-dr <ilidio@yandex.com>
2017-03-23 23:19:51 -0300

    5

--- 1.5.f90
+++ 1.5.f90
@@ -1,37 +1,51 @@
-SUBROUTINE SHIFT(ARR, POS)
-  REAL ARR(:)
+SUBROUTINE SHIFT(RES, POS)
+  INTEGER RES(M)
   INTEGER POS
 
-  DO I=POS, SIZE(ARR)-1
-     ARR(I+1) = ARR(I)
+  DO I=M, POS+1, -1
+     RES(I) = RES(I-1)
   END DO
+  PRINT*, RES
 END SUBROUTINE SHIFT
 
 
 SUBROUTINE MENORES(N, M)
   INTEGER N, M
-  REAL :: ARR(N), RES(M)
-  REAL IPT
-  
+  INTEGER RES(M)
+  INTEGER IPT
+  RES = (/ 0,0,0/)
   DO I=1, N
      READ*, IPT
 
      DO J=M, 1, -1
-        IF ((IPT < RES(I)) .OR. (RES(I) .EQ. 0.0)) THEN
-           CALL SHIFT(RES, I-1)
-           RES(I-1) = IPT
+        IF ((IPT < RES(J)) .AND. (RES(J) .NE. 0.0)) THEN
+        
+           IF (J .EQ. 1) THEN
+              PRINT*, 'IF2 CALLED'
+              CALL SHIFT(RES, 1)
+              RES(1) = IPT
+              EXIT
+           END IF
+           
+           CALL SHIFT(RES, J+1)
+           RES(J+1) = IPT
+           PRINT*, 'IF1 CALLED'
            EXIT
-        END IF
+     END IF
+     
+
      END DO
      
-        
+  PRINT*, 'RES FINAL', RES        
   END DO
-  PRINT*, ARR
+
 
   
 END SUBROUTINE MENORES
 
 
 PROGRAM P
-  CALL SHIFT((\1,2,3,4,5,6\),3)
+  INTEGER RES(3)
+  INTEGER POS
+  CALL MENORES(5,3)
 END PROGRAM P
